61afdd0ea942544b69ba2845d6280de8a9318644
Dilara Ucar <dilara.ucaaar@gmail.com>
2024-04-01 14:42:05 +0000

    Built the BlogPost model

--- hello_world/models.py
+++ hello_world/models.py
@@ -2,6 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+
+# Base model to be used in other models
 class PostBase(models.Model):
     content = models.TextField()
     created_at = models.DateTimeField(auto_now_add=True)
@@ -11,3 +13,24 @@ class PostBase(models.Model):
 
     class Meta:
         abstract = True
+
+
+STATUS = ((0, "Draft"), (1, "Published"))
+
+class BlogPost(PostBase):
+    heading = models.CharField(max_length=100, unique=True)
+    slug = models.SlugField(max_length=100, unique=True)
+    last_updated = models.DateTimeField(auto_now_add=True)
+    writer = models.ForeignKey(
+        User, on_delete=models.CASCADE, related_name="authored_posts"
+    )
+    class Meta:
+        ordering = ["-created_at"]
+
+    def get_url(self):
+        return reverse("post_detail", args=[str(self.slug)])
+
+    def __str__(self):
+        return f"{self.heading} by {self.writer}"
+
+    status = models.IntegerField(choices=STATUS, default=0)
