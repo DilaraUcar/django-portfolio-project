82157ca7acb456b50d133fee4559320dcd6bf65f
Dilara Ucar <dilara.ucaaar@gmail.com>
2024-04-01 13:20:47 +0000

    Create base model

--- hello_world/models.py
+++ hello_world/models.py
@@ -1,3 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class PostBase(models.Model):
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    num_likes = models.IntegerField(default=0)
+    is_pinned = models.BooleanField(default=False)
+
+    class Meta:
+        abstract = True
