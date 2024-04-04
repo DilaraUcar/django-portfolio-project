a4c49d3c7e09b66255e6787382238433a20a5f43
Dilara Ucar <dilara.ucaaar@gmail.com>
2024-04-04 14:54:10 +0000

    Add logo, favicon and index.html page

--- hello_world/views.py
+++ hello_world/views.py
@@ -5,4 +5,5 @@ from .models import BlogPost
 # Create your views here.
 class PostList(generic.ListView):
     queryset = BlogPost.objects.filter(status=1).order_by('-created_at')
-    template_name = "post_details.html"
+    template_name = "index.html"
+    paginate_by = 10
