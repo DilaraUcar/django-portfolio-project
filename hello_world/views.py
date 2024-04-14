738b6a1b18c82f9fb364fc345c09c51f50f64d71
Dilara Ucar <dilara.ucaaar@gmail.com>
2024-04-14 15:14:26 +0000

    Fix buttons styles to make all same styles

--- hello_world/views.py
+++ hello_world/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render, get_object_or_404
 from django.views import generic
+from django.contrib import messages
 from .models import BlogPost
+from .forms import CommentForm
 
 # Create your views here.
 class PostList(generic.ListView):
@@ -26,9 +28,29 @@ def post_detail(request, slug):
 
     queryset = BlogPost.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
+    comments = post.comments.all().order_by("created_at")
+    comment_count = post.comments.all().count()
+
+    if request.method == "POST":
+        comment_form = CommentForm(data=request.POST)
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.writer = request.user
+            comment.post = post
+            comment.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Comment is submitted'
+                )
+
+    comment_form = CommentForm()
 
     return render(
         request,
         "hello_world/post_detail.html",
-        {"post": post},
+        {"post": post,
+        "comments": comments,
+        "comment_count": comment_count,
+        "comment_form": comment_form,
+        },   
     )
