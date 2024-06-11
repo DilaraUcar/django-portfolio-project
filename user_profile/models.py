9651883be59679d81d5ee5fe103934727de3e09b
Dilara Ucar <dilara.ucaaar@gmail.com>
2024-06-11 12:52:48 +0000

    Change image fields and files to use cloudinary

--- user_profile/models.py
+++ user_profile/models.py
@@ -3,13 +3,15 @@ from django.contrib import admin
 from django.contrib.auth.models import User
 from django.db.models.signals import post_save
 from django.dispatch import receiver
+from cloudinary.models import CloudinaryField
+
 
 # Create your models here.
 class Profile(models.Model):
     user = models.OneToOneField(
         User, on_delete=models.CASCADE, related_name='profile')
     about = models.CharField(max_length=500, default="Add bio...")
-    avatar = models.ImageField(upload_to='media/', default='images/nobody.jpg')
+    avatar = CloudinaryField('image', default='images/nobody.jpg')
 
     def __str__(self):
         return f"{self.user.username} Profile"
